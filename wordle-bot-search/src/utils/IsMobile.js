8bf1de368a739b2d000e4fd6c9bc7893a1c739f1
hannah-marcus28 <hannah@movementcooperative.org>
2025-04-20 15:54:46 -0400

    fixing isMobile

--- wordle-bot-search/src/utils/IsMobile.js
+++ wordle-bot-search/src/utils/IsMobile.js
@@ -1,6 +1,8 @@
-export const isMobile = () => {
+const isMobile = () => {
     const mobileRegex = new RegExp(
       `Android|webOS|iPhone|iPad|BlackBerry|Phone|Mobile`
     );
     return navigator.userAgent.search(mobileRegex) >= 0;
   };
+
+export default isMobile;
