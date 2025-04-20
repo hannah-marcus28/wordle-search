546eb46e0000d95c5739229f97870da62ac139ca
hannah-marcus28 <hannah@movementcooperative.org>
2025-04-20 15:51:03 -0400

    creating IsMobile.js

---
+++ wordle-bot-search/src/utils/IsMobile.js
@@ -0,0 +1,6 @@
+export const isMobile = () => {
+    const mobileRegex = new RegExp(
+      `Android|webOS|iPhone|iPad|BlackBerry|Phone|Mobile`
+    );
+    return navigator.userAgent.search(mobileRegex) >= 0;
+  };
